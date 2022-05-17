c969d26161ef2ad947e61420dd1da4dd9a643a49
Amogh Chaubey <chaubeyamogh@gmail.com>
2022-05-17 13:31:36 -0400

    Add Widget + My Own Embed hehe

---
+++ components/Widgets.tsx
@@ -0,0 +1,26 @@
+import { SearchIcon } from '@heroicons/react/outline'
+import React from 'react'
+import { TwitterTimelineEmbed } from 'react-twitter-embed'
+
+function Widgets() {
+  return (
+    <div className='px-2 mt-2'>
+        {/*Search */}
+        <div className ="flex items-center space-x-2 bg-gray-100 p-3 rounded-full">
+            <SearchIcon className='h-5 w-5 text-gray-400' />
+            <input 
+             type="text" 
+             placeholder="Search Twitter" 
+             className="flex-1 outline-none bg-transparent" 
+            />
+        </div>
+        <TwitterTimelineEmbed
+  sourceType="profile"
+  screenName="amoghchaubey"
+  options={{height: 400}}
+/>
+    </div>
+  )
+}
+
+export default Widgets
