eb944bfb2c92a67546aef3fcbd62b582e70cd99a
Amogh Chaubey <chaubeyamogh@gmail.com>
2022-05-17 14:09:25 -0400

    HOLY SHIT LMAOOOO 10,000 CHANGES MY BAD I ADDED SANITY

--- components/Feed.tsx
+++ components/Feed.tsx
@@ -1,12 +1,17 @@
 import { RefreshIcon } from '@heroicons/react/outline'
 import React from 'react'
+import Tweetbox from './Tweetbox'
 
 function Feed() {
   return (
-    <div className='col-span-7 lg:col-span-5'>
+    <div className='col-span-7 lg:col-span-5 border-x'>
         <div className ='flex items-center justify-between'>
             <h1 className='pb-5 p-5 text-xl font-bold'>Home</h1>
             <RefreshIcon className='h-8 w-8 cursor-pointer text-twitter mr-5 mt-5 transition-all duration-500 ease-out hover:rotate-180 active:scale-125"' />
+        </div>
+        <Tweetbox />
+        <div>
+
         </div>
     </div>
   )
--- pages/index.tsx
+++ pages/index.tsx
@@ -7,7 +7,7 @@ import Widgets from '../components/Widgets'
 
 const Home: NextPage = () => {
   return (
-    <div className="">
+    <div className="lg:max-w-6xl mx-auto max-h-screen overflow-hidden">
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
